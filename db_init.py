a027996a5cbe87bc272b809a2aed6bcd2d1d806c
Jesse Akes <akesjesse@gmail.com>
2018-01-20 10:25:21 -0500

    Init db

--- db_init.py
+++ db_init.py
@@ -1,14 +1,31 @@
-# Iniit sqlite db
-
+# Init sqlite db
 import sqlite3
-import future
-
 
 # Main init function
 def db_init():
-    # Create db
-    db = sqlite3.connect()
+    # Create and connect to db
+    db = sqlite3.connect('DB/chow.db')
+    c = db.cursor()
 
+    # Schema stuff 
+    c.execute('''CREATE TABLE IF NOT EXISTS user_profile (
+                uid INTEGER,
+                dietary_restriction TEXT,
+                allergies TEXT )
+                ''')
+
+    c.execute('''CREATE TABLE IF NOT EXISTS choices (
+                uid INTEGER,
+                did INTEGER,
+                cid INTEGER, 
+                point REAL, 
+                rating REAL )''')
+    c.execute('''CREATE TABLE IF NOT EXISTS co_data (
+                did INTEGER,
+                health_info BLOB )''')
+
+    db.commit()
+    db.close()
 
 if __name__ == '__main__':
     db_init()
